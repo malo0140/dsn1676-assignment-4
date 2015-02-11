591044ce6c839a312f7f742a1bf6647ac4b90342
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-02-11 17:01:12 -0500

    made into objects

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var blueberries = {
+
+name: 'Blueberries',    
+fullName: 'Vaccinium corymbosum',
+Growsonshrub: true,
+Energy: 240,
+Carbohydrates: 14.49,
+Protein: 0.74,
+
+};
+
+var grapes = {
+ 
+name: 'Grapes',
+fullName: 'Vitis vinifera',
+Growsonshrub: false,
+Energy: 288,
+Carbohydrates: 18.1,
+Protein: 0.72,
+    
+};
+
+var redcurrant = {
+ 
+name: 'Redcurrant',    
+fullName: 'Ribes rubrum',
+Growsonshrub: true,
+Energy: 234,
+Carbohydrates: 13.8,
+Protein: 1.4,
+    
+};
