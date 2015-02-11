138a310f6909b445ae577990400586282d4e7445
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-02-11 17:50:55 -0500

    adding function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var blueberries = {
 
 name: 'Blueberries',    
-fullName: 'Vaccinium corymbosum',
+labName: 'Vaccinium corymbosum',
 Growsonshrub: true,
 Energy: 240,
 Carbohydrates: 14.49,
@@ -12,7 +12,7 @@ Protein: 0.74,
 var grapes = {
  
 name: 'Grapes',
-fullName: 'Vitis vinifera',
+labName: 'Vitis vinifera',
 Growsonshrub: false,
 Energy: 288,
 Carbohydrates: 18.1,
@@ -23,10 +23,41 @@ Protein: 0.72,
 var redcurrant = {
  
 name: 'Redcurrant',    
-fullName: 'Ribes rubrum',
+labName: 'Ribes rubrum',
 Growsonshrub: true,
 Energy: 234,
 Carbohydrates: 13.8,
 Protein: 1.4,
     
 };
+
+var berrieInfo = function (berrie) {
+    document.write('<h1>' + berrie.name + '</h1>'); 
+    document.write('<dl>');
+    
+    document.write('<dt>labName</dt>');
+    document.write('<dd>' + berrie.labName + '</dd>');
+    
+    document.write('<dt>Grows on shrub?</dt>');
+    document.write('<dd>' + berrie.Growsonshrub + '</dd>');
+    
+    document.write('<dt>Energy</dt>');
+    document.write('<dd>' + berrie.Energy + '</dd>');
+    
+     document.write('<dt>Carbohydrates</dt>');
+    document.write('<dd>' + berrie.Carbohydrates + '</dd>');
+    
+     document.write('<dt>Protein</dt>');
+    document.write('<dd>' + berrie.Protein + '</dd>');
+    
+    document.write('</dl>');
+
+};
+
+
+
+berrieInfo(blueberries);
+berrieInfo(grapes);
+berrieInfo(redcurrant);
+
+    
